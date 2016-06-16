bffd60dd6e3953462df328e04673f4ef713ff2a7
Andrew Gleeson <agleeson@wikia-inc.com>
2016-06-15 18:31:56 -0700

    add script

---
+++ timer.py
@@ -0,0 +1,29 @@
+import requests
+import time
+from tqdm import tqdm
+import sys
+
+N = 20
+URL = 'http://localhost:8080/3035/threads'
+
+times = []
+
+def mean(input):
+	return float(sum(input))/len(input) if len(input) > 0 else float('nan')
+
+if len(sys.argv) != 3:
+	print 'Usage: python timer.py [URL] [sample size]'
+
+print "Timing requests to " + sys.argv[1]
+
+for x in tqdm(xrange(1,int(sys.argv[2])+1)):
+	start = time.time()
+	r = requests.get(URL)
+	r.content  # wait until full content has been transfered
+	roundtrip = time.time() - start
+	times.append(roundtrip)
+
+print "Done!\n\nStatistics:"
+
+print "Average time: %.2f s" % mean(times)
+
